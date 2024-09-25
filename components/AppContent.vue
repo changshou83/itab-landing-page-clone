c7f298a5e0120e207a5ee9ef96656d42287e5422
长寿 <1369548156@qq.com>
2024-09-26 00:43:38 +0800

    feat: update pagination bullet style

--- components/AppContent.vue
+++ components/AppContent.vue
@@ -1,5 +1,6 @@
 <template>
   <swiper
+    :style="`--swiper-pagination-color: ${paginationColor.active};--swiper-pagination-bullet-inactive-color: ${paginationColor.inactive};`"
     :modules="[SwiperPagination, SwiperMousewheel]"
     :direction="'vertical'"
     :allow-touch-move="false"
@@ -9,12 +10,38 @@
     :mousewheel="{
       enabled: true,
     }"
+    @slide-change="setPaginationColor"
   >
     <slot />
   </swiper>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const paginationColor = reactive({
+  active: "#fff",
+  inactive: "#000",
+});
+const colorMap = new Map([
+  ["Widget", { active: "#00a5f2", inactive: "#00a5f2" }],
+  ["Simple", { active: "#00a5f2", inactive: "#00a5f2" }],
+  ["More", { active: "#fff", inactive: "#fff" }],
+  ["Start", { active: "#00a5f2", inactive: "#00a5f2" }],
+]);
+function setPaginationColor(evt: any) {
+  const { activeIndex, slides } = evt;
+  const activeSlide = slides[activeIndex];
+  const swiperSlideIndex = activeSlide.dataset.swiperSlideIndex;
+
+  if (colorMap.has(swiperSlideIndex)) {
+    const { active, inactive } = colorMap.get(swiperSlideIndex)!;
+    paginationColor.active = active;
+    paginationColor.inactive = inactive;
+  } else {
+    paginationColor.active = "#fff";
+    paginationColor.inactive = "#000";
+  }
+}
+</script>
 
 <style>
 .swiper-wrapper {
@@ -34,6 +61,21 @@
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.swiper-pagination {
+  transition: 0.3s opacity;
+  margin-right: 5px;
+  z-index: 1200;
+}
+.swiper-pagination-bullet {
+  transition-duration: 0.3s;
+  transition-timing-function: ease-in-out;
+}
+.swiper-pagination-bullet-active {
+  height: 24px;
+  border-radius: 8px;
+  opacity: 1 !important;
 }
 
 .animate__animated {
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,12 @@
     <AppContent>
       <SwiperSlide virtual-index="Banner"><ContentBanner /></SwiperSlide>
       <SwiperSlide virtual-index="Widget"><ContentWidget /></SwiperSlide>
-      <SwiperSlide><ContentIcon /></SwiperSlide>
-      <SwiperSlide><ContentApp /></SwiperSlide>
-      <SwiperSlide><ContentSimple /></SwiperSlide>
-      <SwiperSlide><ContentVideo /></SwiperSlide>
-      <SwiperSlide><ContentMore /></SwiperSlide>
-      <SwiperSlide><ContentStart /></SwiperSlide>
+      <SwiperSlide virtual-index="Icon"><ContentIcon /></SwiperSlide>
+      <SwiperSlide virtual-index="App"><ContentApp /></SwiperSlide>
+      <SwiperSlide virtual-index="Simple"><ContentSimple /></SwiperSlide>
+      <SwiperSlide virtual-index="Video"><ContentVideo /></SwiperSlide>
+      <SwiperSlide virtual-index="More"><ContentMore /></SwiperSlide>
+      <SwiperSlide virtual-index="Start"><ContentStart /></SwiperSlide>
     </AppContent>
   </div>
 </template>
